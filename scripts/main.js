7351febcb03cf3007aad0dafb4cc7cfb28d1e052
FredN <fnspecter69@users.noreply.github.com>
2015-06-06 10:57:53 -0400

    changes to script and css

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+var main = function(){
+  console.log("Welcome to my GitHub page");
+};
+/*use the .hover() function to change the background-color of the tabs*/
+/*running the page*/
+$(document).ready(main);
