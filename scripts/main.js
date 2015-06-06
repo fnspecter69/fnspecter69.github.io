45c433aadff95baea72189155f676a664ae69905
FredN <fnspecter69@users.noreply.github.com>
2015-06-06 14:48:54 -0400

    add meta dada

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 var main = function(){
   console.log("Welcome to my GitHub page");
+  console.log("Still in development getting real functionallity soon.");
 };
 /*use the .hover() function to change the background-color of the tabs*/
 /*running the page*/
