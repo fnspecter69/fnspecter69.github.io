d11837fb6a6646b17fe938eb2ec380453226a36f
FredN <fnspecter69@users.noreply.github.com>
2015-06-20 14:04:39 -0400

    modify navbar

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,8 @@
 var main = function(){
   console.log("Welcome to my GitHub page");
   console.log("Still in development getting real functionallity soon.");
+
+  /*display date of last build and use either grunt or gulp to do it*/
 };
 /*use the .hover() function to change the background-color of the tabs*/
 /*running the page*/
