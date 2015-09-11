bff417169a25f0a07c6a82dbbc3101c35519f88c
FredN <fnspecter69@users.noreply.github.com>
2015-09-11 12:00:02 -0400

    changes to image slider
    use Bootstrap carousel instead of building on from scratch

--- js/main.js
+++ js/main.js
@@ -1,33 +1,5 @@
 'use strict';
 
-$(function(){
-    //configuration
-    var width = 720;
-    var animationSpeed = 1000;
-    var pause = 3000;
-    var currentSlide = 1;
-    //cache DOM
-    var $slider = $('#slider');
-    var $slideContainer = $slider.find('slides');
-    var $slides = $slideContainer.find('.slide');
-
-    setInterval(function(){
-      $slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function(){
-          currentSlide++;
-          if(currentSlide === $slides.length){
-              currentSlide = 1;
-              $slideContainer.css('margin-left', 0);
-          }
-      });
-    }, pause);
-    //setInterval
-      //animate margin-left
-      //if it's last slide, go to position 1 (0px);
-
-    //listen for mouseenter and pause
-    //resume on mouseenter
-});
-
 var main = function(){
   console.log(new Date());
   console.log("Welcome to my GitHub page");
