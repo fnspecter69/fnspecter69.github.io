bb61c4c9e1e5890dfdf23cede592d28c411a35c1
FredN <fnspecter69@users.noreply.github.com>
2016-02-06 10:57:41 -0500

    navbar right

--- js/main.js
+++ js/main.js
@@ -7,6 +7,16 @@ var main = function(){
 
   /*display date of last build and use either grunt or gulp to do it*/
   /*adding selecter to change color*/
+  
+  //user node.js HTTPS server to make a dynamic site on home
+  var https = ('https');
+  http.get("http://www.google.com/index.html", function(res) {
+  console.log("Got response: " + res.statusCode);
+  // consume response body
+  res.resume();
+}).on('error', function(e) {
+  console.log("Got error: " + e.message);
+});
 };
 
 $(document).ready(main);
